0a83f0941fd45b38afd7670f0583bd139edaf5d7
wangwenchao <wangwenchao2@pset.suntec.net>
2016-05-25 16:56:44 +0800

    modify hello.php

--- hello.php
+++ hello.php
@@ -1,5 +1,15 @@
 <?php
 echo '菜鸟教程：www.runoob.com';
 echo 'www.runoob.com';
+echo '菜鸟教程：www.runoob.com';
+echo 'www.runoob.com';
+echo '菜鸟教程：www.runoob.com';
+echo 'www.runoob.com';
+echo '菜鸟教程：www.runoob.com';
+echo 'www.runoob.com';
+echo '菜鸟教程：www.runoob.com';
+echo 'www.runoob.com';
+echo '菜鸟教程：www.runoob.com';
+echo 'www.runoob.com';
 ?>
 
